bf2726a0716ba329938b898d46925050e3025f5d
hizumi <nyatel@yandex.ru>
2023-04-26 12:51:48 +0400

    исправленное решение с автозаполнением

--- bonus/Program.cs
+++ bonus/Program.cs
@@ -3,12 +3,12 @@
 // 	Пересечением двух отрезка 1 и 2 могут быть  - отрезок, точка или ничего
 // 	С клавиатуры вводим х1, x2, х3, x4 и выводим ответ - границы пересечений отрезков, общую точку или «не пересекаются».
 
-Console.WriteLine("Введите первые координаты: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите вторые координаты: ");
-int x3 = Convert.ToInt32(Console.ReadLine());
-int x4 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите первые координаты: ");
+int x1 = new Random().Next(-1000, 1000);
+int x2 = new Random().Next(-1000, 1000);
+//Console.WriteLine("Введите вторые координаты: ");
+int x3 = new Random().Next(-1000, 1000);
+int x4 = new Random().Next(-1000, 1000);
 
 int result = 0;
 
@@ -24,7 +24,11 @@ if(x1 < x3){
     } else{
     result = x4 -x3;
     }
-}else result = x4 - x1;
+}else if(x2 < x4){
+    result = x2 - x1;
+}else {
+    result = x4 - x1;
+}
 
 if(result <=0 ){
     Console.WriteLine("Отрезки не пересекаются");
