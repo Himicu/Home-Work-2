7f074fc4ad9c0744489ed7f334587cd5d88bc413
hizumi <nyatel@yandex.ru>
2023-04-26 15:17:51 +0400

    исправлено под условия задачи

--- bonus/Program.cs
+++ bonus/Program.cs
@@ -3,10 +3,17 @@
 // 	Пересечением двух отрезка 1 и 2 могут быть  - отрезок, точка или ничего
 // 	С клавиатуры вводим х1, x2, х3, x4 и выводим ответ - границы пересечений отрезков, общую точку или «не пересекаются».
 
-//Console.WriteLine("Введите первые координаты: ");
+void CheckForNegativ(int a, int b){
+int result = a - b;
+if(result < 0){
+    Console.WriteLine("Нет пересечений");
+} else if(result == 0){
+    Console.WriteLine($"Пересечение в точке {a}");
+} else Console.WriteLine($"пересечение в точках ({b},{a})");
+
+}
 int x1 = new Random().Next(-1000, 1000);
 int x2 = new Random().Next(-1000, 1000);
-//Console.WriteLine("Введите вторые координаты: ");
 int x3 = new Random().Next(-1000, 1000);
 int x4 = new Random().Next(-1000, 1000);
 
@@ -18,16 +25,17 @@ if(x1 > x2){
 if(x3 > x4){
     (x3, x4) = (x4, x3);
 }
+Console.WriteLine($"Получились координаты: ({x1}, {x2}) ({x3}, {x4})");
 if(x1 < x3){
     if(x2 < x4){
-        result = x2 - x3;
+        CheckForNegativ(x2, x3);
     } else{
-    result = x4 -x3;
+   CheckForNegativ(x4 ,x3);
     }
 }else if(x2 < x4){
-    result = x2 - x1;
+    CheckForNegativ(x2, x1);
 }else {
-    result = x4 - x1;
+    CheckForNegativ(x4, x1);
 }
 
 if(result <=0 ){
